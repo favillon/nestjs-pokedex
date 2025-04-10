80ed50cd457bb51a5eee0140520c7411a0541c71
favillon <favillon@hotmail.com>
2025-04-09 21:57:14 -0500

    Insercion con el await de uno a una la peticion

---
+++ src/seed/seed.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
+import { PokeResponse } from './interfaces/poke-response.interface';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+
+@Injectable()
+export class SeedService {
+
+  private readonly axios: AxiosInstance = axios;
+
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>
+  ) {}
+
+  async executeSeed() {
+
+    await this.pokemonModel.deleteMany({});
+    
+    const {data} = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650');
+    data.results.forEach( async({name, url })=> {
+      const segments = url.split('/');
+      const no = +segments[segments.length - 2];
+      const pokemon = await this.pokemonModel.create({
+        name,
+        no
+      });
+    
+    })
+    return 'Seed executed successfully';
+  }
+}
